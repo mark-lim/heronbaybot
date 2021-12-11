7049c7be28db4d3792931e1e14194fcc8820ab4c
XLasagna99 <93477013+XLasagna99@users.noreply.github.com>
2021-12-11 20:36:26 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -16,10 +16,20 @@ logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %m(messag
                     level = logging.INFO)
 
 #Start function
-startstr = "Welcome to the Heron Bay Facilities Booking Bot! How may I help you today?"
+startstr = "Welcome to the Heron Bay Facilities Booking Bot! How may I help you today?\n/gym   : Book a slot at the gym \n/bbq   : Book the barbeque pit\n/tennis: Book the tennis court"
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id = update.effective_chat.id,
                              text = startstr)
+
+#Other functions
+def gym(update: Update, context: CallbackContext):
+    update.message.reply_text("Which day would you like to book the gym for?")
+
+def tennis(update: Update, context: CallbackContext):
+    update.message.reply_text("Which day would you like to book the tennis court for?")
+
+def bbq(update: Update, context: CallbackContext):
+    update.message.reply_text("Which day would you like to book the bbq pit for?")
 
 #Allow the start function to be called everytime the bot receives a message containing "/start"
 start_handler = Commandhandler('start', start)
