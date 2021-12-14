c38d4a1540f8d01acb734a7f92d752f7365657a4
XLasagna99 <93477013+XLasagna99@users.noreply.github.com>
2021-12-14 11:45:48 +0800

    Add files via upload

--- whispering-wildwood-52220/bot.py
+++ whispering-wildwood-52220/bot.py
@@ -1,82 +1,77 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
+from telegram.ext import Updater
+import time
 import logging
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import os
-PORT = int(os.environ.get('PORT', 5000))
+#Import Updater and Dispatcher classs to fetch new updates from telegramp
+updater = Updater(token = "2001966813:AAE2fZOT96v6vseK7-zL1aHKp5_p0MiVP5A", use_context = True)
+dispatcher = updater.dispatcher
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
+#Create log to check back for errors
+logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %m(message)s', 
+                    level = logging.INFO)
 logger = logging.getLogger(__name__)
+PORT = int(os.environ.get('PORT', 5000))
 TOKEN = '2001966813:AAE2fZOT96v6vseK7-zL1aHKp5_p0MiVP5A'
 
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
+#Create states 
+FIRST, SECOND = 1,2
+#Start function
 def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    #Record in log that user has started convo
+    user = update.message.from_user
+    logger.info("User %s started the conversation.", user.first_name)
 
+    #Building inline keyboard with multiple rows
+    keyboard = [
+        [InlineKeyboardButton("Gym", callback_data="1"),
+         InlineKeyboardButton("Tennis Courts", callback_data="2")],
+         [InlineKeyboardButton("BBQ Pits", callback_data='3'),
+         InlineKeyboardButton("Exit", callback_data="4")]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    #Send messages with start
+    update.message.reply_text("Welcome to the Heron Bay Facilities Booking Bot!")
+    time.sleep(0.5)
+    update.message.reply_text("How may I help you today?")
+    time.sleep(0.5)
+    update.message.reply_text(
+        "Select one of the following: ",
+        reply_markup = reply_markup
+    )
 
 def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
+    update.message.reply_text("For any enquiries contact the facilities manager!")
 
 def error(update, context):
-    """Log Errors caused by Updates."""
+    #Log errors that are caused by updates
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
-
-    # Get the dispatcher to register handlers
+    updater = Updater(TOKEN, use_context = True)
+    
+    #Allow dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
+    #Indicate what happens if I input different commands into Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
 
-    # Start the Bot
+    #Start bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
+
     updater.bot.setWebhook('https://whispering-wildwood-52220.herokuapp.com/' + TOKEN)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
